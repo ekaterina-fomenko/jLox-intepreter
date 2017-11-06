009597b029668058a00e6b46ebb9a0bf50f1b323
Ekaterina Fomenko <katfom@gmail.com>
2017-11-06 15:48:41 +0300

    Add token types

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JLox intepreter
+# JLox interpreter
 
 Interpreter for Lox language https://github.com/munificent/craftinginterpreters
 These are learning tasks from the book http://www.craftinginterpreters.com/
--- src/main/java/com/craftinginterpreters/lox/TokenType.java
+++ src/main/java/com/craftinginterpreters/lox/TokenType.java
@@ -1,7 +1,30 @@
 package com.craftinginterpreters.lox;
 
-/**
- * Created by ekaterina on 06.11.17.
- */
-public class TokenType {
+enum TokenType {
+    /*
+    * Single-character tokens
+    */
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    /*
+    * One or two character tokens
+    */
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    /*
+    * Literals
+    */
+    IDENTIFIER, STRING, NUMBER,
+
+    /*
+    * Keywords
+    */
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
 }
