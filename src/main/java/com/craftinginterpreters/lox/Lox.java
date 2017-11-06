98126803fcfb23501dc13d39c98aee4e18094c50
Ekaterina Fomenko <katfom@gmail.com>
2017-11-06 15:48:02 +0300

    Add error reporting

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -13,6 +13,9 @@ import java.util.List;
  */
 
 public class Lox {
+    private static volatile boolean hadError = false;
+    public static final int EXIT_CODE = 65;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -29,6 +32,7 @@ public class Lox {
     public static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+        if (hadError) System.exit(EXIT_CODE);
     }
 
     private static void runPrompt() throws IOException {
@@ -38,6 +42,7 @@ public class Lox {
         for (; ; ) {
             System.out.print("> ");
             run(reader.readLine());
+            hadError = false;
         }
     }
 
@@ -51,6 +56,22 @@ public class Lox {
         for (Token token : tokens) {
             System.out.println(token);
         }
+    }
+
+    /*
+    * Handle errors
+    */
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    /*
+    *Construct report about the error
+    */
+    static private void report(int line, String where, String message) {
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
     }
 
 }
---
+++ src/main/java/com/craftinginterpreters/lox/TokenType.java
@@ -0,0 +1,7 @@
+package com.craftinginterpreters.lox;
+
+/**
+ * Created by ekaterina on 06.11.17.
+ */
+public class TokenType {
+}
