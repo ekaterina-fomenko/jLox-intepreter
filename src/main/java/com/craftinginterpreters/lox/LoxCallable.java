14ac613c62de8b9197687c0824d083231821606e
Ekaterina Fomenko <katfom@gmail.com>
2017-12-31 15:03:18 +0300

    Add call type error treatment

--- src/main/java/com/craftinginterpreters/lox/LoxCallable.java
+++ src/main/java/com/craftinginterpreters/lox/LoxCallable.java
@@ -5,5 +5,9 @@ import com.craftinginterpreters.lox.interpreter.Interpreter;
 import java.util.List;
 
 public interface LoxCallable {
+
+    //Number of arguments that function expects
+    int arity();
+
     Object call(Interpreter interpreter, List<Object> arguments);
 }
