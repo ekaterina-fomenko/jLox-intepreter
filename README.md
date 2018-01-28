8e66c0d3d2141dc19b5a7224f03fab6cf2b2ce08
Ekaterina Fomenko <katfom@gmail.com>
2018-01-29 00:15:19 +0300

    Add error handling for super word

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 Interpreter for Lox language https://github.com/munificent/craftinginterpreters.
 
 These are learning tasks from the book http://www.craftinginterpreters.com/.
+
+**In interpreter were implemented** :
+
+* tokens and lexing
+* abstract syntax trees
+* recursive descent parsing
+* prefix and infix expressions
+* runtime representation of objects
+* interpreting code using the Visitor pattern
+* lexical scope
+* environment chains for storing variables
+* control flow
+* functions with parameters
+* closures
+* static variable resolution and error detection
+* classes
+* constructors
+* fields
+* methods
+* inheritance
--- src/main/java/com/craftinginterpreters/lox/tools/Resolver.java
+++ src/main/java/com/craftinginterpreters/lox/tools/Resolver.java
@@ -25,7 +25,8 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     private enum ClassType {
         NONE,
-        CLASS
+        CLASS,
+        SUBCLASS
     }
 
     private final Interpreter interpreter;
@@ -118,6 +119,7 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         ClassType enclosingClass = currentClass;
         currentClass = ClassType.CLASS;
         if (stmt.superclass != null) {
+            currentClass = ClassType.SUBCLASS;
             resolve(stmt.superclass);
             beginScope();
             scopes.peek().put("super", true);
@@ -230,6 +232,12 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitSuperExpr(Expr.Super expr) {
+        if (currentClass == ClassType.NONE) {
+            Lox.error(expr.keyword, "Cannot use 'super' outside of a class.");
+        } else if (currentClass != ClassType.SUBCLASS) {
+            Lox.error(expr.keyword, "Cannot use 'super' in a class with no superclass.");
+        }
+
         resolveLocal(expr, expr.keyword);
         return null;
     }
